0bbc5d912d4ff62cf63225a05ed9f04e484b4e88
benparker <bp48371@students.westport.k12.ct.us>
2014-09-15 12:22:56 -0400

    This is my First Commit. I am not done yet, cowboy!

---
+++ controllers/simple_routes.rb
@@ -0,0 +1,17 @@
+#this is file simple_routes.rb
+
+get '/home' do
+  redirect 'index.html'
+end
+
+get '/dynamic' do
+  erb :main
+end
+
+get '/anchors' do
+  redirect 'anchor.html'
+end
+
+get '/video' do
+  redirect 'video.html'
+end
